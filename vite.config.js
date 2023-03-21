7c55f682eb978736d4da42e87447438786897c41
64318333+BalazsMakrai@users.noreply.github.com <64318333+BalazsMakrai@users.noreply.github.com>
2023-03-21 14:05:45 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,14 @@
-// vite.config.js
-const path = require('path')
-const { defineConfig } = require('vite')
+import { fileURLToPath, URL } from "url";
 
-module.exports = defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: 'MyLib',
-      fileName: (format) => `my-lib.${format}.js`
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
-})
+  },
+});
